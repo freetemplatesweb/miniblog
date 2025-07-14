51db0a7ada755e22fc1b29db4371ed22d09ae056
freetemplatesweb <freetemplatesoficial@gmail.com>
2025-07-13 22:17:40 -0300

    configurando as rotas no arquivo app

--- aula1/src/App.js
+++ aula1/src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
+//importação do dados do router dom
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//importação das paginas
+import Home from './pages/Home/Home';
+import Sobre from './pages/Sobre/Sobre';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Mini blog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+          <Route path='/' element={<Home />}/>
+           <Route path='/sobre' element={<Sobre />}/>
+          </Routes>
+        </div>
+      
+      </BrowserRouter>
+
     </div>
   );
 }
