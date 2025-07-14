aa192f4f60a1284471f931ca022677e08223ac7c
freetemplatesweb <freetemplatesoficial@gmail.com>
2025-07-13 22:57:34 -0300

    inserindo o NavLink e Footer no arquivo app

--- aula1/src/App.js
+++ aula1/src/App.js
@@ -5,19 +5,22 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //importação das paginas
 import Home from './pages/Home/Home';
 import Sobre from './pages/Sobre/Sobre';
-
+//importando os componetes
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <div className="container">
           <Routes>
           <Route path='/' element={<Home />}/>
            <Route path='/sobre' element={<Sobre />}/>
           </Routes>
         </div>
-      
+        <Footer />
       </BrowserRouter>
 
     </div>
